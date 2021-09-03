cd41a4fa8d65717e7225174f8f4e32fb7c9171a3
Rafika <RTuremis@hotmail.co.uk>
2021-09-03 15:50:09 +0100

    Make restaurant endpoints

--- Vibe/places/urls.py
+++ Vibe/places/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('restaurants/', views.RestaurantList.as_view()),
+    path('restaurants/<int:restaurant_id>', views.RestaurantDetail.as_view()),
 ]
--- Vibe/places/views.py
+++ Vibe/places/views.py
@@ -2,12 +2,50 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from .models import Restaurant
+from .serializers import RestaurantSerializer
+from django.http import Http404
 
 
 # Create your views here.
 class RestaurantList(APIView):
-    permission_classes = [IsAuthenticated,]
+    # permission_classes = [IsAuthenticated,]
     def get(self, request, format=None):
         restaurants = Restaurant.objects.all()
         serializer = RestaurantSerializer(restaurants, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+            serializer = RestaurantSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class RestaurantDetail(APIView):
+
+    def get_object(self, restaurant_id):
+        try:
+            return Restaurant.objects.get(pk=restaurant_id)
+        except Restaurant.DoesNotExist:
+            raise Http404
+
+    def get(self, request, restaurant_id, format=None):
+        restaurant = self.get_object(restaurant_id)
+        serializer = RestaurantSerializer(restaurant)
+        return Response(serializer.data)
+
+    def put(self, request, restaurant_id, format=None):
+        restaurant = self.get_object(restaurant_id)
+        serializer = RestaurantSerializer(restaurant, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, restaurant_id, format=None):
+        restaurant = self.get_object(restaurant_id)
+        restaurant.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
