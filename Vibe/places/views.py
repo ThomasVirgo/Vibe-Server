fdb0a877b4fcb7da4876956746a8fe4ca69964c8
polinamoore <p_gerasimenko@yahoo.com>
2021-09-03 10:48:47 +0100

    add RestaurantList to views

--- Vibe/places/views.py
+++ Vibe/places/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
+class RestaurantList(APIView):
+    permission_classes = [IsAuthenticated,]
+    def get(self, request, format=None):
+        restaurants = Restaurant.objects.all()
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return Response(serializer.data)
