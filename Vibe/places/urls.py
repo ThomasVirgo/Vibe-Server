de3e84d033c603f25a65d3aedab5be8e90f932b3
Rafika <RTuremis@hotmail.co.uk>
2021-09-04 14:19:46 +0100

    Add endpoints for event reviews

--- Vibe/places/urls.py
+++ Vibe/places/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('restaurant-reviews/', views.RestaurantReviewList.as_view()),
     path('restaurant-reviews/<int:review_id>/', views.RestaurantReviewDetail.as_view()), #refactor so restaurants/<int:restaurant_id>/<int:review_id>
     path('events/', views.EventList.as_view()),
+    path('events/<int:event_id>', views.EventDetail.as_view()),
+    path('event-reviews/', views.EventReviewList.as_view()),
+    path('event-reviews/<int:review_id>/', views.EventReviewDetail.as_view()), #refactor so events/<int:event_id>/<int:review_id>
 ]
--- Vibe/places/views.py
+++ Vibe/places/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import Restaurant, RestaurantReview, Event
-from .serializers import RestaurantSerializer, RestaurantReviewSerializer, EventSerializer
+from .models import Restaurant, RestaurantReview, Event, EventReview
+from .serializers import RestaurantSerializer, RestaurantReviewSerializer, EventSerializer, EventReviewSerializer
 from django.http import Http404
 
 
@@ -106,3 +106,71 @@ class EventList(APIView):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EventDetail(APIView):
+
+    def get_object(self, event_id):
+        try:
+            return Event.objects.get(pk=event_id)
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get(self, request, event_id, format=None):
+        event = self.get_object(event_id)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def put(self, request, event_id, format=None):
+        event = self.get_object(event_id)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, event_id, format=None):
+        event = self.get_object(event_id)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#Event Review views
+class EventReviewList(APIView):
+    # permission_classes = [IsAuthenticated,]
+    def get(self, request, format=None):
+        reviews = EventReview.objects.all()
+        serializer = EventReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+            serializer = EventReviewSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EventReviewDetail(APIView):
+
+    def get_object(self, review_id):
+        try:
+            return EventReview.objects.get(pk=review_id)
+        except EventReview.DoesNotExist:
+            raise Http404
+
+    def get(self, request, review_id, format=None):
+        review = self.get_object(review_id)
+        serializer = EventReviewSerializer(review)
+        return Response(serializer.data)
+
+    def put(self, request, review_id, format=None):
+        review = self.get_object(review_id)
+        serializer = EventReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, review_id, format=None):
+        review = self.get_object(review_id)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
