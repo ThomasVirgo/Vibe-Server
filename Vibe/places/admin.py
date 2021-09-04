0eef1a6f83f819b8250525ac750c731dd7e2c297
Rafika <RTuremis@hotmail.co.uk>
2021-09-04 13:24:39 +0100

    Add working endpoint for events category

--- Vibe/places/admin.py
+++ Vibe/places/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Restaurant, RestaurantReview
+from .models import Restaurant, RestaurantReview, Event
 
 # Register your models here.
 
 admin.site.register(Restaurant)
 admin.site.register(RestaurantReview)
+admin.site.register(Event)
--- Vibe/places/views.py
+++ Vibe/places/views.py
@@ -2,12 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import Restaurant, RestaurantReview
-from .serializers import RestaurantSerializer, RestaurantReviewSerializer
+from .models import Restaurant, RestaurantReview, Event
+from .serializers import RestaurantSerializer, RestaurantReviewSerializer, EventSerializer
 from django.http import Http404
 
 
 # Create your views here.
+
+# Restaurant views
 class RestaurantList(APIView):
     # permission_classes = [IsAuthenticated,]
     def get(self, request, format=None):
@@ -89,3 +91,18 @@ class RestaurantReviewDetail(APIView):
         review = self.get_object(review_id)
         review.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Event views
+class EventList(APIView):
+    # permission_classes = [IsAuthenticated,]
+    def get(self, request, format=None):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+            serializer = EventSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
