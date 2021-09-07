6eb85bfcd514390e3955d6a2d58957ac7ba69b33
Tom Virgo <tomcvirgo@gmail.com>
2021-09-07 09:59:13 +0100

    add endpoint to get reviews for specific restaurant

--- Vibe/places/urls.py
+++ Vibe/places/urls.py
@@ -5,10 +5,11 @@ from django.urls import path
 urlpatterns = [
     path('restaurants/', views.RestaurantList.as_view()),
     path('restaurants/<int:restaurant_id>', views.RestaurantDetail.as_view()),
+    path('restaurants/reviews/<int:restaurant_id>/', views.GetRestaurantReviews.as_view()), 
     path('restaurant-reviews/', views.RestaurantReviewList.as_view()),
-    path('restaurant-reviews/<int:review_id>/', views.RestaurantReviewDetail.as_view()), #refactor so restaurants/<int:restaurant_id>/<int:review_id>
+    path('restaurant-reviews/<int:review_id>/', views.RestaurantReviewDetail.as_view()), 
     path('events/', views.EventList.as_view()),
     path('events/<int:event_id>', views.EventDetail.as_view()),
     path('event-reviews/', views.EventReviewList.as_view()),
-    path('event-reviews/<int:review_id>/', views.EventReviewDetail.as_view()), #refactor so events/<int:event_id>/<int:review_id>
+    path('event-reviews/<int:review_id>/', views.EventReviewDetail.as_view()), 
 ]
